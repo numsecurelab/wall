42a4d92ac3ca3f8ed811f261055b02f98e5d6f26
chyngyz <chyngyz.omurov@gmail.com>
2019-12-06 11:22:40 +0600

    For testing invalid txs

--- bitcoincore/src/main/java/io/horizontalsystems/bitcoincore/DustCalculator.kt
+++ bitcoincore/src/main/java/io/horizontalsystems/bitcoincore/DustCalculator.kt
@@ -8,15 +8,15 @@ class DustCalculator(dustRelayTxFee: Int, val sizeCalculator: TransactionSizeCal
 
     fun dust(type: ScriptType): Int {
         // https://github.com/bitcoin/bitcoin/blob/c536dfbcb00fb15963bf5d507b7017c241718bf6/src/policy/policy.cpp#L18
-
-        var size = sizeCalculator.outputSize(type)
-
-        if (type.isWitness) {
-            size += sizeCalculator.inputSize(ScriptType.P2WPKH) + sizeCalculator.witnessSize(ScriptType.P2WPKH) / 4
-        } else {
-            size += sizeCalculator.inputSize(ScriptType.P2PKH)
-        }
-
-        return size * minFeeRate
+        return 0
+//        var size = sizeCalculator.outputSize(type)
+//
+//        if (type.isWitness) {
+//            size += sizeCalculator.inputSize(ScriptType.P2WPKH) + sizeCalculator.witnessSize(ScriptType.P2WPKH) / 4
+//        } else {
+//            size += sizeCalculator.inputSize(ScriptType.P2PKH)
+//        }
+//
+//        return size * minFeeRate
     }
 }
